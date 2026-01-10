136d1319e028b95eb9734e2bf1c01f125c3871df
Kkiiiiiii <rikizhakaria@gmail.com>
2026-01-10 17:14:24 +0700

    term condition

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+Route::post('/daftar/sineas', [PageController::class, 'store'])->name('daftarSineas');
 
 
